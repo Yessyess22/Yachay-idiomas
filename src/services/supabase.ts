3d7fd799b87b50f10f03fe9bf9d8f2764fb214ec
Yessyess22 <yessyess3689@gmail.com>
2026-09-21 20:17:21 -0400

    correccion del service para la apk

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsYWNlaG9sZGVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6MjAwMDAwMDAwMH0.placeholder';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://pcjkblllqokyhomlwloo.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjamtibGxscW9reWhvbWx3bG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NzUzMDAsImV4cCI6MjAwMDAwMDAwMH0.tWqeNN_qQJ5fVuliCsWD_Xg3sdK9mZzwC3hMNI6KsgY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
